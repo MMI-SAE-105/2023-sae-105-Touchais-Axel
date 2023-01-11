2c99cab8cf6dc7bbf88905baf6a58ad8ce4c4e90
Touchais-Axel <axel.touchais@edu.univ-fcomte.fr>
2023-01-11 21:06:58 +0100

    menu fonctionnel (première version)

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,38 @@
-const hamburger = document.getElementById("hamburger");
+const hamburgerButton = document.getElementById("hamburger");
 const menuContent = document.getElementById("menu-content");
+const closeButton = document.getElementById("close-button");
 
-hamburger.addEventListener("click", () => {
+hamburgerButton.addEventListener("click", () => {
   menuContent.classList.toggle("open");
 });
+
+closeButton.addEventListener("click", () => {
+  menuContent.classList.remove("open");
+});
+
+const toggleButtons = document.querySelectorAll(".toggle-button");
+
+for (let button of toggleButtons) {
+    button.addEventListener("click", e => {
+        let subMenu = e.target.nextElementSibling;
+        subMenu.classList.toggle("show");
+    });
+}
+
+const toggleButtons2 = document.querySelectorAll(".toggle-button2");
+
+for (let button of toggleButtons) {
+    button.addEventListener("click", e => {
+        let subMenu2 = e.target.nextElementSibling;
+        subMenu2.classList.toggle("show");
+    });
+}
+
+const toggleButtons3 = document.querySelectorAll(".toggle-button3");
+
+for (let button of toggleButtons) {
+    button.addEventListener("click", e => {
+        let subMenu3 = e.target.nextElementSibling;
+        subMenu3.classList.toggle("show");
+    });
+}
